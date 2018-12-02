00fa83676ea7927ac15d957067f1ebadc0625857
Jakob Bussas <jakobbu@gmx.de>
2018-12-02 21:14:07 +0100

    Day02 Part2

--- Day02/Day02/Program.cs
+++ Day02/Day02/Program.cs
@@ -10,6 +10,7 @@ namespace Day02
             string[] Input = File.ReadAllLines("../../input.txt");
 
             Part1(Input); // 5434
+            Part2(Input); // agimdjvlhedpsyoqfzuknpjwt
         }
 
         private static void Part1(string[] Input)
@@ -25,7 +26,8 @@ namespace Day02
                 {
                     for (int j = 0; j <= i; j++)
                     {
-                        if(Id[i] == Id[j]) {
+                        if (Id[i] == Id[j])
+                        {
                             Amounts[j]++;
                             break;
                         }
@@ -35,11 +37,14 @@ namespace Day02
 
                 bool Two = false;
                 bool Three = false;
-                foreach(var element in Amounts)
+                foreach (var element in Amounts)
                 {
-                    if(element == 2) {
+                    if (element == 2)
+                    {
                         Two = true;
-                    } else if (element == 3) {
+                    }
+                    else if (element == 3)
+                    {
                         Three = true;
                     }
                 }
@@ -51,6 +56,47 @@ namespace Day02
             int Checksum = Twos * Threes;
 
             Console.WriteLine(Checksum);
+        }
+
+        private static void Part2(string[] Input)
+        {
+            for (int i = 0, n = Input.Length; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    // check for inequality
+                    bool Inequal = false;
+                    bool Wrong = false;
+                    for (int k = 0; k < Input[i].Length; k++)
+                    {
+                        if (Input[i][k] != Input[j][k])
+                        {
+                            if (Inequal)
+                            {
+                                Wrong = true;
+                                break;
+                            }
+                            else
+                            {
+                                Inequal = true;
+                            }
+                        }
+                    }
+                    if (!Wrong)
+                    {
+                        // format
+                        string Result = "";
+                        for (int l = 0; l < Input[i].Length; l++)
+                        {
+                            if (Input[i][l] == Input[j][l])
+                            {
+                                Result += Input[i][l];
+                            }
+                        }
+                        Console.WriteLine(Result);
+                    }
+                }
+            }
         }
     }
 }
