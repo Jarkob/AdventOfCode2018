7c507902d46f5472bc5424dac9600f275a03e68c
Jakob Bussas <jakobbu@gmx.de>
2018-12-02 21:01:59 +0100

    Day02 Part1

---
+++ Day02/Day02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Day02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] Input = File.ReadAllLines("../../input.txt");
+
+            Part1(Input); // 5434
+        }
+
+        private static void Part1(string[] Input)
+        {
+            int Twos = 0;
+            int Threes = 0;
+
+            foreach (var Id in Input)
+            {
+                int[] Amounts = new int[Id.Length];
+
+                for (int i = 0, n = Amounts.Length; i < n; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if(Id[i] == Id[j]) {
+                            Amounts[j]++;
+                            break;
+                        }
+                    }
+                }
+
+
+                bool Two = false;
+                bool Three = false;
+                foreach(var element in Amounts)
+                {
+                    if(element == 2) {
+                        Two = true;
+                    } else if (element == 3) {
+                        Three = true;
+                    }
+                }
+
+                if (Two) { Twos++; }
+                if (Three) { Threes++; }
+            }
+
+            int Checksum = Twos * Threes;
+
+            Console.WriteLine(Checksum);
+        }
+    }
+}
