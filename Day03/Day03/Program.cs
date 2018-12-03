d3a7c8abee97f2f8ccc641e74a0fe609c4ebfdb7
Jakob Bussas <jakobbu@gmx.de>
2018-12-03 22:03:58 +0100

    Day03 Part2

--- Day03/Day03/Program.cs
+++ Day03/Day03/Program.cs
@@ -10,13 +10,8 @@ namespace Day03
         {
             string[] Input = File.ReadAllLines("../../input.txt");
 
-            //Input = null;
-            //Input = new string[] {"#1 @ 1,3: 4x4",
-            //    "#2 @ 3,1: 4x4",
-            //    "#3 @ 5,5: 2x2"
-            //};
-
             Part1(Input); // 110383
+            Part2(Input); // 129
         }
 
         private static void Part1(string[] Input)
@@ -68,6 +63,67 @@ namespace Day03
             }
 
             Console.WriteLine(Overlaps);
+        }
+
+        private static void Part2(string[] Input)
+        {
+            // format input
+            Claim[] Claims = new Claim[Input.Length];
+            for (int i = 0, n = Input.Length; i < n; i++)
+            {
+                string[] Parts = Input[i].Split(' ');
+                int Id = Convert.ToInt32(Parts[0].Substring(1));
+                int PaddingLeft = Convert.ToInt32(Parts[2].Split(',')[0]);
+                int PaddingTop = Convert.ToInt32(Parts[2].Split(',')[1].Split(':')[0]);
+                int Width = Convert.ToInt32(Parts[3].Split('x')[0]);
+                int Height = Convert.ToInt32(Parts[3].Split('x')[1]);
+
+                Claims[i] = new Claim(Id, PaddingLeft, PaddingTop, Width, Height);
+            }
+
+            // initialize grid
+            int[][] Grid = new int[1000][];
+            for (int i = 0, n = Grid.Length; i < n; i++)
+            {
+                Grid[i] = new int[1000];
+            }
+
+            // track intactness
+            bool[] Intact = new bool[Input.Length];
+            for (int i = 0, n = Intact.Length; i < n; i++)
+            {
+                Intact[i] = true;
+            }
+
+            // distribute claims
+            foreach (var Claim in Claims)
+            {
+                for (int i = Claim.PaddingLeft; i < Claim.PaddingLeft + Claim.Width; i++)
+                {
+                    for (int j = Claim.PaddingTop; j < Claim.PaddingTop + Claim.Height; j++)
+                    {
+                        if (Grid[i][j] == 0)
+                        {
+                            Grid[i][j] = Claim.Id;
+                        }
+                        else
+                        {
+                            Intact[Grid[i][j] - 1] = false;
+                            Intact[Claim.Id - 1] = false;
+                        }
+                    }
+                }
+            }
+
+            // find intact
+            for (int i = 0, n = Intact.Length; i < n; i++)
+            {
+                if (Intact[i])
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+            }
         }
     }
 }
