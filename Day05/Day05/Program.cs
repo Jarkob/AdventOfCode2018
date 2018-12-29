853acb8ad3c9ba333f214faa38a4992df67921de
Jakob Bussas <jakobbu@gmx.de>
2018-12-29 15:12:49 +0100

    finished Day05

--- Day05/Day05/Program.cs
+++ Day05/Day05/Program.cs
@@ -9,6 +9,12 @@ namespace Day05
         public static void Main(string[] args)
         {
             string Input = File.ReadAllText("../../input.txt");
+            Console.WriteLine(Part1(Input)); // Part1: 9808
+            Console.WriteLine(Part2(Input));
+        }
+
+        private static int Part1(string Input)
+        {
             List<char> Letters = new List<char>(Input.ToCharArray());
             for (int i = 0, n = Letters.Count; i < n - 1; i++)
             {
@@ -21,7 +27,36 @@ namespace Day05
                 }
             }
             string Result = new string(Letters.ToArray());
-            Console.WriteLine(Result.Length); // Part1: 9808
+            return Result.Length;
+        }
+
+        private static int Part2(string Input)
+        {
+            int[] Lengths = new int[26];
+            List<char> Letters = new List<char>(Input.ToCharArray());
+            List<char> FilteredLetters;
+            for (int i = 65; i < 91; i++)
+            {
+                FilteredLetters = new List<char>();
+                foreach (var Element in Letters)
+                {
+                    if (Element != i && Element != i + 32)
+                    {
+                        FilteredLetters.Add(Element);
+                    }
+                }
+                Lengths[i - 65] = Part1(new string(FilteredLetters.ToArray()));
+            }
+
+            int MinLength = Int32.MaxValue;
+            foreach (var Element in Lengths)
+            {
+                if (Element < MinLength)
+                {
+                    MinLength = Element;
+                }
+            }
+            return MinLength; // 9462 too high
         }
     }
 }
