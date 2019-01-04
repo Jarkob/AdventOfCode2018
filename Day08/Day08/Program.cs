00214f7a7d182626012a5ae99e7ed5a50ffb40bd
Jakob Bussas <jakobbu@gmx.de>
2019-01-04 22:54:56 +0100

    Day08 Part1

---
+++ Day08/Day08/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day08
+{
+    /// <summary>
+    /// @author Jakob Bussas
+    /// @author ValdasTheUnique
+    /// </summary>
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string Input = File.ReadAllText("../../input.txt");
+
+            // debug
+            //Input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+            int[] Numbers = Array.ConvertAll(Input.Split(' '), int.Parse);
+            int Index = 0;
+            TreeElement Root = GetTree(Numbers, ref Index);
+            Console.WriteLine(Root.GetSum()); // Part1: 45750
+        }
+
+        private static TreeElement GetTree(int[] Numbers, ref int Index)
+        {
+            TreeElement Element = new TreeElement();
+            int ChildNodes = Numbers[Index++];
+            int MetaDataEntries = Numbers[Index++];
+            for (int i = 0; i < ChildNodes; i++)
+            {
+                Element.next.Add(GetTree(Numbers, ref Index));
+            }
+            for (int i = 0; i < MetaDataEntries; i++)
+            {
+                Element.MetaData.Add(Numbers[Index++]);
+            }
+            return Element;
+        }
+    }
+}
---
+++ Day08/Day08/TreeElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Day08
+{
+    public class TreeElement
+    {
+        public TreeElement()
+        {
+            this.next = new List<TreeElement>();
+            this.MetaData = new List<int>();
+        }
+
+        public List<TreeElement> next { get; set; }
+        public TreeElement prev { get; set; }
+        public List<int> MetaData { get; set; }
+
+        public int GetSum()
+        {
+            int Sum = 0;
+            foreach (var Element in this.next)
+            {
+                Sum += Element.GetSum();
+            }
+
+            foreach (var MetaDataEntry in this.MetaData)
+            {
+                Sum += MetaDataEntry;
+            }
+
+            return Sum;
+        }
+    }
+}
