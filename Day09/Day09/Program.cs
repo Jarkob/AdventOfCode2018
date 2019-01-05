f985023de892345ff25ded2d635c0d0ba3b13d15
Jakob Bussas <jakobbu@gmx.de>
2019-01-05 21:58:51 +0100

    Day09 Part1

---
+++ Day09/Day09/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Day09
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // 416 players; last marble is worth 71617 points
+
+            int NumberOfPlayers = 416;
+            int NumberOfMarbles = 71617;
+
+            //NumberOfMarbles = 7999; // FIXME
+            //NumberOfPlayers = 13; // FIXME
+
+            int[] PlayerScores = new int[NumberOfPlayers];
+            int Player = 0;
+
+            Ring Marbles = new Ring();
+
+            for (int Marble = 1; Marble <= NumberOfMarbles; Marble++)
+            {
+                if (Marble % 23 == 0)
+                {
+                    PlayerScores[Player] += Marble;
+                    PlayerScores[Player] += Marbles.RemoveCounterClockWise();
+                }
+                else
+                {
+                    Marbles.InsertClockWise(Marble);
+                }
+                //Console.WriteLine(Marbles.ToString());
+
+                Player = Player == NumberOfPlayers - 1 ? 0 : Player + 1;
+            }
+
+            // find highscore
+            int HighScore = 0;
+            foreach (var Score in PlayerScores)
+            {
+                if (Score > HighScore)
+                {
+                    HighScore = Score;
+                }
+            }
+
+            Console.WriteLine("Highscore: {0}", HighScore); // Part1: 436720
+        }
+    }
+}
---
+++ Day09/Day09/Ring.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day09
+{
+    public class Ring
+    {
+        public Ring()
+        {
+            this.Marbles = new List<int>();
+            this.Index = 0;
+            this.Marbles.Add(0);
+        }
+
+        public int Index;
+        public List<int> Marbles { get; set; }
+
+        public void InsertClockWise(int Marble)
+        {
+            if (this.Index == this.Marbles.Count - 1)
+            {
+                this.Marbles.Insert(1, Marble);
+                this.Index = 1;
+            }
+            else if (this.Index == this.Marbles.Count - 2)
+            {
+                this.Marbles.Add(Marble);
+                this.Index = this.Marbles.Count - 1;
+            }
+            else
+            {
+                Index += 2;
+                this.Marbles.Insert(Index, Marble);
+            }
+        }
+
+        public int RemoveCounterClockWise()
+        {
+            Index -= 7;
+            int Value = -1;
+            if (Index < 0)
+            {
+                Index = this.Marbles.Count + Index;
+            }
+            Value = this.Marbles[Index];
+            this.Marbles.RemoveAt(Index);
+            return Value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder Builder = new StringBuilder();
+            foreach (var Element in this.Marbles)
+            {
+                Builder.Append(string.Format("{0,4}", Element));
+            }
+            return Builder.ToString();
+        }
+    }
+}
