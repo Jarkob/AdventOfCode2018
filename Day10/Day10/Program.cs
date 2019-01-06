eeaf166a3669092f3b69aeed098cca4dc6b3f826
Jakob Bussas <jakobbu@gmx.de>
2019-01-06 12:20:51 +0100

    finished Day10

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -10,44 +9,9 @@ namespace Day10
         public static void Main(string[] args)
         {
             // Part1: PHLGRNFK
+            // Part2: 10407
 
             string[] Input = File.ReadAllLines("../../input.txt");
-            // FIXME debug
-            //Input = null;
-            //Input = new string[]
-            //{
-            //    "position=< 9,  1> velocity=< 0,  2>",
-            //    "position=< 7,  0> velocity=<-1,  0>",
-            //    "position=< 3, -2> velocity=<-1,  1>",
-            //    "position=< 6, 10> velocity=<-2, -1>",
-            //    "position=< 2, -4> velocity=< 2,  2>",
-            //    "position=<-6, 10> velocity=< 2, -2>",
-            //    "position=< 1,  8> velocity=< 1, -1>",
-            //    "position=< 1,  7> velocity=< 1,  0>",
-            //    "position=<-3, 11> velocity=< 1, -2>",
-            //    "position=< 7,  6> velocity=<-1, -1>",
-            //    "position=<-2,  3> velocity=< 1,  0>",
-            //    "position=<-4,  3> velocity=< 2,  0>",
-            //    "position=<10, -3> velocity=<-1,  1>",
-            //    "position=< 5, 11> velocity=< 1, -2>",
-            //    "position=< 4,  7> velocity=< 0, -1>",
-            //    "position=< 8, -2> velocity=< 0,  1>",
-            //    "position=<15,  0> velocity=<-2,  0>",
-            //    "position=< 1,  6> velocity=< 1,  0>",
-            //    "position=< 8,  9> velocity=< 0, -1>",
-            //    "position=< 3,  3> velocity=<-1,  1>",
-            //    "position=< 0,  5> velocity=< 0, -1>",
-            //    "position=<-2,  2> velocity=< 2,  0>",
-            //    "position=< 5, -2> velocity=< 1,  2>",
-            //    "position=< 1,  4> velocity=< 2,  1>",
-            //    "position=<-2,  7> velocity=< 2, -2>",
-            //    "position=< 3,  6> velocity=<-1, -1>",
-            //    "position=< 5,  0> velocity=< 1,  0>",
-            //    "position=<-6,  0> velocity=< 2,  0>",
-            //    "position=< 5,  9> velocity=< 1, -2>",
-            //    "position=<14,  7> velocity=<-2,  0>",
-            //    "position=<-3,  6> velocity=< 2, -1>"
-            //};
 
             // parse input
             int[][] Points = new int[Input.Length][];
@@ -101,7 +65,6 @@ namespace Day10
             }
 
             Console.WriteLine("MinExpansion at {0}: {1}", MinExpansionIndex, MinExpansion);
-
 
             // get max min
             int MaxXFinal = Int32.MinValue;
