5aaccbf308e052b3b42b2c7da77099af8fc2ef7b
Jakob Bussas <jakobbu@gmx.de>
2019-01-06 23:10:21 +0100

    finished Day12

--- Day12/Day12/Program.cs
+++ Day12/Day12/Program.cs
@@ -10,31 +10,8 @@ namespace Day12
         {
             string[] Input = File.ReadAllLines("../../input.txt");
             string InitialState = Input[0].Split(' ')[2];
-            // FIXME debug
-            //InitialState = "#..#.#..##......###...###";
-
-            //Input = null;
-            //Input = new string[]
-            //{
-            //    "...## => #",
-            //    "..#.. => #",
-            //    ".#... => #",
-            //    ".#.#. => #",
-            //    ".#.## => #",
-            //    ".##.. => #",
-            //    ".#### => #",
-            //    "#.#.# => #",
-            //    "#.### => #",
-            //    "##.#. => #",
-            //    "##.## => #",
-            //    "###.. => #",
-            //    "###.# => #",
-            //    "####. => #"
-            //};
-
-            bool[] Pots = new bool[InitialState.Length + 250];
-            // 200 is the new 0
-            for (int i = 200, n = Pots.Length - 50; i < n; i++)
+            bool[] Pots = new bool[InitialState.Length + 350];
+            for (int i = 200, n = Pots.Length - 150; i < n; i++)
             {
                 Pots[i] = InitialState[i - 200] == '#';
             }
@@ -51,17 +28,8 @@ namespace Day12
                 ), Input[j][9] == '#'));
             }
 
-            for (int k = 0; k < 20; k++)
+            for (int k = 0; k < 130; k++)
             {
-                // FIXME debug
-                // print
-                for (int i = 190; i < 240; i++)
-                {
-                    Console.Write(Pots[i] ? '#' : '.');
-                }
-                Console.WriteLine();
-
-
                 // save old pots
                 bool[] NewPots = new bool[Pots.Length];
                 for (int i = 0; i < Pots.Length; i++)
@@ -92,7 +60,45 @@ namespace Day12
                     Sum += m - 200;
                 }
             }
-            Console.WriteLine(Sum); // Part1: 3494
+            Console.WriteLine("sum: " + Sum); // Part1: 3494
+
+            // ugly
+            for (int k = 0; k < 1; k++)
+            {
+                // save old pots
+                bool[] NewPots = new bool[Pots.Length];
+                for (int i = 0; i < Pots.Length; i++)
+                {
+                    NewPots[i] = Pots[i];
+                }
+
+                // execute rules
+                for (int l = 2, n = Pots.Length - 2; l < n; l++)
+                {
+                    if (Rules.ContainsKey(
+                    new Tuple<bool, bool, bool, bool, bool>(Pots[l - 2], Pots[l - 1], Pots[l], Pots[l + 1], Pots[l + 2])))
+                    {
+                        NewPots[l] = Rules[new Tuple<bool, bool, bool, bool, bool>(Pots[l - 2], Pots[l - 1], Pots[l], Pots[l + 1], Pots[l + 2])];
+                    }
+                }
+
+                Pots = null;
+                Pots = NewPots;
+            }
+            // difference
+            // get sum
+            long NewSum = 0;
+            for (int m = 0, n = Pots.Length; m < n; m++)
+            {
+                if (Pots[m])
+                {
+                    NewSum += m - 200;
+                }
+            }
+            long Difference = NewSum - Sum;
+            Console.WriteLine("difference: " + Difference); // 57
+            NewSum = Sum + (50000000000 - 130) * 57;
+            Console.WriteLine(NewSum); // Part2; 2850000002454
         }
     }
 }
